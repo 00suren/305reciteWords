f7169cd7a3d9e9444b58b17a6041f319ac05dd7a
su ren <0000suren@gmail.com>
2020-05-31 19:31:35 +0800

    换了个图标

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -1,42 +1,7 @@
 const app = getApp()
 
 Page({
-  onLoad:function(){
-    wx.getSetting({
-      success: res => {
-        console.log(res.authSetting);
-        if (res.authSetting['scope.userInfo']) {
-          wx.login({
-            success: () => {
-              wx.getUserInfo({
-                success: res => {
-                  this.globalData.userInfo = res.userInfo
-                  console.log(res)
-                },
-                fail: err => {
-                  console.log(err)
-                }
-              })
-            },
-            fail: err => {
-              console.log(err)
-            }
-          })
-        }
-        else {
-          console.log("登录失败");
-          wx.showToast({
-            title: '请授予权限',
-            icon: 'none'
-          })
-
-        }
-      },
-      fail: err => {
-        console.err;
-      }
-    })
-   
+  onLoad:function(){ 
   },
 
   showMyWord: function () {
@@ -61,7 +26,46 @@ Page({
       url: './feedback/feedback'
     })
   },
-  test: function(){
- 
+
+
+  onGetUserInfo: function () {
+    wx.getSetting({
+      success: res => {
+        console.log(res.authSetting);
+        if (res.authSetting['scope.userInfo']) {
+          wx.login({
+            success: () => {
+              wx.getUserInfo({
+                success: res => {
+                  console.log(res)
+                  this.setData({
+                    avatarUrl:res.userInfo.avatarUrl,
+                    nickname:res.userInfo.nickName
+                  })
+                },
+                fail: err => {
+                  console.log(err)
+                }
+              })
+            },
+            fail: err => {
+              console.log(err)
+            }
+          })
+        }
+        else {
+          console.log("登录失败");
+          wx.showToast({
+            title: '请授予权限',
+            icon: 'none'
+          })
+
+        }
+      },
+      fail: err => {
+        console.err;
+      }
+    })
   }
+
 })
