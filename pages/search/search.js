0242e30e672759005c5152c386b0a638ecdc9b7a
su ren <0000suren@gmail.com>
2020-06-07 18:43:35 +0800

    添加每日一句功能（未完成，找不到https的接口）

--- pages/search/search.js
+++ pages/search/search.js
@@ -2,6 +2,20 @@ Page({
   data: {
   },
 
+onShow:function(){
+  wx.request({
+    url: 'http://open.iciba.com/dsapi/',
+    data:{},
+    method:'GET',
+    success:res=>{
+      console.log(res);
+      this.setData({
+        sentence:res.content
+      })
+    }
+  })
+
+},
   search: e => {
     let content = e.detail.value
     wx.request({
